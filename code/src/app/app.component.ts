cc6c0397126bb1885453643582c386976c041cb0
Asit Parida <asitparida@live.in>
2017-05-04 01:08:39 +0530

    Adding Side Nav

--- code/src/app/app.component.ts
+++ code/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  AfterViewInit
+} from '@angular/core';
+import { DEFS } from './app.defs';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works well!';
+
+export class AppComponent implements AfterViewInit {
+  appLoaderId: string = DEFS.GenerateId();
+  appDrawerId: string = DEFS.GenerateId();
+
+  ngAfterViewInit() {
+    let self = this;
+    let _loader: HTMLElement = document.getElementById(self.appLoaderId);
+    let _drawer: HTMLElement = document.getElementById(self.appDrawerId);
+    setTimeout(() => {
+    _drawer.style.display = 'FLEX';
+      $(_loader).animate({ 'opacity': 0 }, 300, () => {
+        _loader.style.display = 'NONE';
+        _drawer.style.opacity = '1';
+      });
+    }, 2500)
+  }
+
 }
---
+++ code/src/app/app.defs.ts
@@ -0,0 +1,6 @@
+import * as _ from 'underscore';
+export module DEFS {
+    export function GenerateId() {
+        return _.uniqueId('_COMPONENT_');
+    };
+}
